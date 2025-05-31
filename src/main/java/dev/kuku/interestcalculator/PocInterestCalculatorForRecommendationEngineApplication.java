ca163a672fde5674c8a468f36eca13a67e94ec6b
kuchukboramdebbarma <kuchukboromd@gmail.com>
2025-05-31 16:40:30 +0530

    Initial commit

---
+++ src/main/java/dev/kuku/interestcalculator/PocInterestCalculatorForRecommendationEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.kuku.interestcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocInterestCalculatorForRecommendationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocInterestCalculatorForRecommendationEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kuku/interestcalculator/PocInterestCalculatorForRecommendationEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kuku.interestcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocInterestCalculatorForRecommendationEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
